ee9df92b6aa02b199791b4d3ba6df7bef4159a53
Saurabh1826 <saurabh_mallela@yahoo.com>
2019-09-24 08:51:59 -0700

    m

--- TextEditorV2/src/Test.java
+++ TextEditorV2/src/Test.java
@@ -3,9 +3,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("SM");
-		System.out.println("Espen");
-		System.out.println("Evan");
-		System.out.println("Candem");
 	}
 
 }
