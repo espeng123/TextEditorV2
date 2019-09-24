15002a2b9bd08f750144f2a09056cd786ef448e6
Saurabh1826 <saurabh_mallela@yahoo.com>
2019-09-24 08:53:03 -0700

    m

--- TextEditorV2/src/Test.java
+++ TextEditorV2/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("We are making minimal progress bois");
+		System.out.println("Bois, we are making minimal progress");
 	}
 
 }
