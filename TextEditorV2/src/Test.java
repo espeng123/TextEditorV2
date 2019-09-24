e7de0d28e8af30dcb815810f48ccbc4bc0800eb7
Saurabh1826 <saurabh_mallela@yahoo.com>
2019-09-24 08:42:41 -0700

    m

--- TextEditorV2/src/Test.java
+++ TextEditorV2/src/Test.java
@@ -2,41 +2,9 @@
 public class Test {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		System.out.println("Test");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		System.out.println("Hello");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println("SM");
 	}
 
 }
