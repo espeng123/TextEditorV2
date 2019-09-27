e4df490704bd5dc3c747c65017fead3e48f5bdff
Espen <espeng123@icloud.com>
2019-09-27 08:56:43 -0700

    h

--- TextEditorV2/src/FrameTest.java
+++ TextEditorV2/src/FrameTest.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class FrameTest implements ActionListener{
+public class FrameTest{
 
 	public static void main(String[] args) {
 		
