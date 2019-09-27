2bea7a9faee82c158d387c3937d14cc0ad5b653c
Espen <espeng123@icloud.com>
2019-09-27 09:15:27 -0700

    h

--- TextEditorV2/src/FrameTest.java
+++ TextEditorV2/src/FrameTest.java
@@ -1,27 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class FrameTest{
+	
+	static JFrame frame = new JFrame("GUI");
+	static JLabel label = new JLabel("<html></html>",JLabel.LEFT);
 
 	public static void main(String[] args) {
 		
-		 JFrame frame = new JFrame("GUI");
 		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 frame.setSize(800,800);
 		 frame.getContentPane().setBackground(Color.black);
 		 frame.setLayout(new BorderLayout());
 		 
-		 String words = "<html><p>This is a long paragraph and I want it to break on its own.  " + 
-				    "This is a long paragraph and I want it to break on its own.  " +
-				    "This is a long paragraph and I want it to break on its own.  " +
-				    "This is a long paragraph and I want it to break on its own.</p></html>";
+		 JPanel panel = new JPanel();
+		 frame.getContentPane().add(panel);
+		 panel.addKeyListener(new KeyListener() {
+			 @Override
+			 public void keyTyped(KeyEvent e) {}
+
+			 @Override
+			 public void keyReleased(KeyEvent e) {}
+
+			 @Override
+			 public void keyPressed(KeyEvent e) {
+				 typeText(e.getKeyChar());
+				 System.out.println("Pressed " + e.getKeyChar());
+			 }
+		 });
+		 panel.setFocusable(true);
+
 		 
-		 
-		 JLabel label = new JLabel(words,JLabel.LEFT);
 		 JLabel label1 = new JLabel("<html> <br> <br></html>");
 		 JLabel label2 = new JLabel(" ");
 		 JLabel label3 = new JLabel("             ");
@@ -41,4 +52,15 @@ public class FrameTest{
 		 frame.setVisible(true);
 
 	}
+	
+	public static void typeText(char keyTyped)
+	{
+		String previousText = label.getText();
+		previousText = previousText.substring(0,previousText.length()-7);
+		label.setText(previousText+keyTyped+"</html>");
+		frame.repaint();
+	}
+	
+	
+	
 }
