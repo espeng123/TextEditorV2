4bb26f1e3d98c26fcbc5db2e20f67819ff6a16cb
Espen <espeng123@icloud.com>
2019-09-27 08:56:07 -0700

    d,s;afk h

--- TextEditorV2/src/FrameTest.java
+++ TextEditorV2/src/FrameTest.java
@@ -1,13 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class FrameTest {
+public class FrameTest implements ActionListener{
 
 	public static void main(String[] args) {
+		
 		 JFrame frame = new JFrame("GUI");
 		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 frame.setSize(800,800);
 		 frame.getContentPane().setBackground(Color.black);
+		 frame.setLayout(new BorderLayout());
 		 
 		 String words = "<html><p>This is a long paragraph and I want it to break on its own.  " + 
 				    "This is a long paragraph and I want it to break on its own.  " +
@@ -16,6 +22,10 @@ public class FrameTest {
 		 
 		 
 		 JLabel label = new JLabel(words,JLabel.LEFT);
+		 JLabel label1 = new JLabel("<html> <br> <br></html>");
+		 JLabel label2 = new JLabel(" ");
+		 JLabel label3 = new JLabel("             ");
+		 JLabel label4 = new JLabel("             ");
 		 label.setVerticalAlignment(JLabel.TOP);
 		 Dimension size = label.getPreferredSize();
 		 System.out.println(size);
@@ -23,10 +33,12 @@ public class FrameTest {
 		 label.setForeground(Color.white);
 		 label.setFont(new Font("Courier", Font.PLAIN, 14));
 		 
-		 frame.add(label);
-		 frame.setLayout(null);
+		 frame.add(label,BorderLayout.CENTER);
+		 frame.add(label1,BorderLayout.NORTH);
+		 frame.add(label2,BorderLayout.SOUTH);
+		 frame.add(label3,BorderLayout.EAST);
+		 frame.add(label4,BorderLayout.WEST);
 		 frame.setVisible(true);
 
 	}
-
 }
